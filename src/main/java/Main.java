9696be4afaa31be4ecd1e2af84e75f90e0db59cd
Mohammad Talim <talimmohammad116@gmail.com>
2025-02-20 05:23:11 +0000

    fix: move prompt to REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    System.out.print("$ ");
-
     Scanner scanner = new Scanner(System.in);
 
     while (true) {
+      System.out.print("$ ");
       String input = scanner.nextLine();
 
       if (input.contains("exit")) {
-          break;
+        break;
       }
 
       System.out.println(input + ": command not found");
